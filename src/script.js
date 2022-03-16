f7874fa58f28a4ad6d3c1802052e0577d4fd9a7c
Kammy <pixelsinthestars@gmail.com>
2022-03-16 00:04:57 -0400

    format days
    added icons, and 5 day forecast, rounded temp as well

--- src/script.js
+++ src/script.js
@@ -21,24 +21,43 @@ function formatDate(date) {
   return `${day}, ${hours}:${minutes}`;
 }
 
+function formatDay(timeStamp) {
+let date = new Date(timeStamp * 1000)
+let day = date.getDay()
+let days = ["Sun","Mon", "Tues", "Wed", "Thurs","Fri", "Sat"]
+
+return days[day]
+}
+
 function displayForecast(response) {
-  console.log(response.data)
+  let forecast = response.data.daily
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days = ["Thurs", "Fri", "Sat", "Sun", "Mon"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
+
+  
+
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6 ) {
+      forecastHTML =
+        forecastHTML +
+        `
     <div class="col-2">
-      <div class="weather-forecast-date">${day}</div>
-        <img src="http://openweathermap.org/img/wn/10d@2x.png"/>
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+      
+        <img src="http://openweathermap.org/img/wn/${
+          forecastDay.weather[0].icon
+        }@2x.png"/>
         <div class="weather-forecast-temperature">
-          <span class="weather-high">18°</span> |
-          <span class="weather-low">12°</span>
+          <span class="weather-high">${Math.round(
+            forecastDay.temp.max
+          )}°</span> |
+          <span class="weather-low">${Math.round(forecastDay.temp.min)}°</span>
         </div>
      </div>`;
+    }
   });
   forecastHTML = forecastHTML + `</div>`;
   forecastElement.innerHTML = forecastHTML;
